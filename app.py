256d04974821b2d89d5c5023afd4db4cecc93834
Garima Hansa <107038186+HanArima@users.noreply.github.com>
2025-01-04 14:35:35 +0000

    added the HTML templates

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
-  return "hello, World!"
+def home():
+  return render_template("base.html")
 
+@app.route("/detect")
+def detect():
+  return render_template("detect.html")
+
+@app.route("/revisit")
+def revisit():
+  return render_template("revisit.html")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
